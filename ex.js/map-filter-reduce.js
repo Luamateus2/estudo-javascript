da9008057bdfc315cd85e582bd4bac03d2d1b893
luana <luanakarolineliramateus2021@gmail.com>
2024-10-21 17:48:43 -0300

    add

--- ex.js/map-filter-reduce.js
+++ ex.js/map-filter-reduce.js
@@ -8,3 +8,22 @@ console.log(numberMultipliedByTwo);
 
 
 //
+
+const ages = [8,13,27,30,22,48]
+//filter filtra
+const agesPar =ages.filter(
+    function(age){
+      return age % 2 ===0
+    }
+) 
+console.log(agesPar)
+
+const agesImpar = ages.filter(function(item){
+    return item %2 !=0
+})
+console.log(agesImpar)
+
+const soma= ages.reduce(function(age,acumuLator){
+    return acumuLator+age
+})
+console.log(soma)
---
+++ ex.js/objects.js
@@ -0,0 +1,10 @@
+const person ={
+    firstName:'Felipe',
+    lastName : 'Rocha',
+    age: '21',
+    hobbies :['Assistir','Jogar Futebol','ler']
+}
+console.log(person)
+console.log(person.firstName)
+console.log(person.hobbies)
+console.log(person.hobbies[1])
